8d0bfa5929dedeaf22057f85557d9c906316e16f
drewgraham <drew@drewg.co.uk>
2025-08-08 07:33:33 +0100

    test: capture XHR responses

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -44,13 +44,21 @@ function createWin(responseData) {
     setTimeout,
     clearTimeout,
     fetch: async () => new ResponseStub(responseData),
-    XMLHttpRequest: function () {}
+    XMLHttpRequest: function () {
+      this._listeners = {};
+    }
   };
 
   win.XMLHttpRequest.prototype = {
     open() {},
-    send() {},
-    addEventListener() {}
+    send() {
+      this.responseText = JSON.stringify(responseData);
+      const fn = this._listeners['load'];
+      if (fn) fn.call(this);
+    },
+    addEventListener(event, cb) {
+      this._listeners[event] = cb;
+    }
   };
 
   win.triggerMutation = () => observerCallback && observerCallback();
@@ -112,4 +120,29 @@ test('uses timeout when value never appears', { concurrency: false }, async () =
   assert.equal(field.firstSeenMs, null);
   assert.ok(field.lastCheckedMs >= 30);
   assert.ok(field.lastCheckedMs < 100);
+});
+
+test('records XHR responses when value appears in DOM', { concurrency: false }, async () => {
+  const win = createWin({ ping: 'pong' });
+  windowHandler(win);
+
+  commands.startApiRecording({ timeoutMs: 100 });
+
+  const xhr = new win.XMLHttpRequest();
+  xhr.open('GET', 'https://example.com/api');
+  xhr.send();
+
+  win.document.body.innerText = 'pong';
+  win.triggerMutation();
+
+  await new Promise((r) => setTimeout(r, 0));
+
+  const report = commands.stopApiRecording();
+  assert.equal(report.length, 1);
+  const field = report[0].fields[0];
+  assert.equal(field.path, 'ping');
+  assert.equal(field.value, 'pong');
+  assert.ok(field.firstSeenMs > 0);
+  assert.equal(field.firstSeenMs, field.lastCheckedMs);
+  assert.ok(field.firstSeenMs < 100);
 });
